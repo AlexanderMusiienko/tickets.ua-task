003860169743ad76f92c8b922c86addeb414a407
AlexanderMusiienko <all.myen@gmail.com>
2016-11-13 13:39:33 +0200

    test.js code refactoring

--- tests/test.js
+++ tests/test.js
@@ -18,92 +18,118 @@ var flights = {
   price: null
 }
 
+function setDate(browser) {
+  browser.execute(
+      function(data) {
+        document.getElementById('departure_date').removeAttribute('readonly');
+        document.getElementById('departure_date_1').removeAttribute('readonly');
+      },[]
+  );
+
+  browser.assert.visible('#departure_date')
+  .click('#departure_date')
+  .setValue('#departure_date', obj.departure_date)
+  .assert.visible('#departure_date_1')
+  .click('#departure_date_1')
+  .setValue('#departure_date_1', obj.return_date);
+}
+
+function setDepart(browser) {
+  browser.clearValue('input[data-direction=departure]')
+  .assert.visible('input[data-direction=departure]')
+  .setValue('input[data-direction=departure]', obj.origin)
+  .pause(1000)
+  .click('#ui-id-1 .ui-menu-item');
+}
+
+function setArrival(browser) {
+  browser.clearValue('input[data-direction=arrival]')
+  .assert.visible('input[data-direction=arrival]')
+  .setValue('input[data-direction=arrival]', obj.destination)
+  .pause(1000)
+  .click('#ui-id-2 .ui-menu-item');
+}
+
+function getPrice(browser) {
+  browser.getText('.item-block__footer > .price-block', function(result) {
+    var price = result.value.replace(/\D/g, '');
+    flights.price = price;
+  });
+}
+
+function getDepart(browser) {
+  browser.getText('section.item-block:nth-of-type(1) .segment-block:nth-of-type(1) .ac_name', function(result) {
+    var airline = result.value;
+    flights.depart.airline = airline;
+  })
+
+  .elements('css selector', 'section.item-block:nth-of-type(1) .segment-block:nth-of-type(1) .flight-maininfo ul:nth-child(1) li:nth-of-type(1)', function(result)  {
+      result.value.forEach(function(el){
+        var id = el.ELEMENT;
+        browser.elementIdText(id, function(res){
+          var flight =  res.value.replace(/[^\w-]+/g, '');
+          flights.depart.flight.push(flight);
+        })
+      })
+  });
+}
+
+function getArrival(browser) {
+  browser.getText('section.item-block:nth-of-type(1) .segment-block:nth-of-type(2) .ac_name', function(result) {
+    var airline = result.value;
+    flights.arrival.airline = airline;
+  })
+
+  .elements('css selector', 'section.item-block:nth-of-type(1) .segment-block:nth-of-type(2) .flight-maininfo ul:nth-child(1) li:nth-of-type(1)', function(result)  {
+      result.value.forEach(function(el){
+        var id = el.ELEMENT;
+        browser.elementIdText(id, function(res){
+          var flight =  res.value.replace(/[^\w-]+/g, '');
+          flights.arrival.flight.push(flight);
+        })
+      })
+  })
+}
+
 module.exports = {
+
+  //first test
   'Fill input fields of flight' : function (browser) {
     browser.url('https://tickets.ua/')
-
     .waitForElementVisible('body', 1000)
-    .execute(
-        function(data) {
-          document.getElementById('departure_date').removeAttribute('readonly');
-          document.getElementById('departure_date_1').removeAttribute('readonly');
-        },[]
-    )
+    .assert.elementPresent('#round_trip[checked]');
 
-    .elementPresent('#round_trip[checked]')
+    setDepart(browser);
+    setArrival(browser);
+    setDate(browser);
 
-    .clearValue('input[data-direction=departure]')
-    .visible('input[data-direction=departure]')
-    .setValue('input[data-direction=departure]', obj.origin)
-    .pause(1000)
-    .click('#ui-id-1 .ui-menu-item')
-
-    .clearValue('input[data-direction=arrival]')
-    .visible('input[data-direction=arrival]')
-    .setValue('input[data-direction=arrival]', obj.destination)
-    .pause(1000)
-    .click('#ui-id-2 .ui-menu-item')
-
-    .visible('#departure_date')
-    .click('#departure_date')
-    .setValue('#departure_date', obj.departure_date)
-
-    .visible('#departure_date_1')
-    .click('#departure_date_1')
-    .setValue('#departure_date_1', obj.return_date)
-
-    .click('input[data-action=avia-search_start-search-btn]')
+    browser.click('input[data-action=avia-search_start-search-btn]')
 
   },
 
+  //second test
   'Get flights data' : function (browser) {
     browser.waitForElementVisible('label[for=UAH]', 17000)
-    .click('label[for=UAH]')
+    .click('label[for=UAH]');
 
-    .getText('.item-block__footer > .price-block', function(result) {
-      var price = result.value.replace(/\D/g, '');
-      flights.price = price;
-    })
+    getPrice(browser);
+    getDepart(browser);
+    getArrival(browser);
 
-    .getText('section.item-block:nth-of-type(1) .segment-block:nth-of-type(1) .ac_name', function(result) {
-      var airline = result.value;
-      flights.depart.airline = airline;
-    })
-
-    .elements('css selector', 'section.item-block:nth-of-type(1) .segment-block:nth-of-type(1) .flight-maininfo ul:nth-child(1) li:nth-of-type(1)', function(result)  {
-        result.value.forEach(function(el){
-          var id = el.ELEMENT;
-          browser.elementIdText(id, function(res){
-            var flight =  res.value.replace(/[^\w-]+/g, '');
-            flights.depart.flight.push(flight);
-          })
-        })
-    })
-
-    .getText('section.item-block:nth-of-type(1) .segment-block:nth-of-type(2) .ac_name', function(result) {
-      var airline = result.value;
-      flights.arrival.airline = airline;
-    })
-
-    .elements('css selector', 'section.item-block:nth-of-type(1) .segment-block:nth-of-type(2) .flight-maininfo ul:nth-child(1) li:nth-of-type(1)', function(result)  {
-        result.value.forEach(function(el){
-          var id = el.ELEMENT;
-          browser.elementIdText(id, function(res){
-            var flight =  res.value.replace(/[^\w-]+/g, '');
-            flights.arrival.flight.push(flight);
-          })
-        })
-    })
-    .pause(1000)
+    browser.pause(1000)
     .click('body', function() {
-      fs.writeFile(outputFile, JSON.stringify(flights, null, 2), function(err) {
-        if (err) {
-          return console.log('Oh, was some error ' + err);
-        }
-        console.log('The file was saved!');
-      });
+      saveData(flights);
     })
     .end()
   }
 
 };
+
+function saveData (data) {
+  fs.writeFile(outputFile, JSON.stringify(data, null, 2), function(err) {
+    if (err) {
+      return console.log('Oh, was some error ' + err);
+    }
+    console.log('The file was saved!');
+  });
+}
